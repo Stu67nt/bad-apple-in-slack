18cfa0536637865935037dad1b2c0fbb89b7cb45
Tanzil Chowdhury <tanzilchowdhury2014@gmail.com>
2026-04-26 13:13:00 +0100

    Added the cat photos, added the slop generator code and the badapple video

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ import random
 import time
 import numpy
 import cv2 as cv
+import edge_tts
+import asyncio
+from moviepy import VideoFileClip, vfx, AudioFileClip
+import re
+import threading
+
+load_dotenv()
+app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+
 
 def frame_to_gs(frame):
 	"""
@@ -15,6 +24,7 @@ def frame_to_gs(frame):
 	"""
 	# Vector Calc to convert whole frame in 1 go. Storing frame as unsigned 16 bit int
 	return ((frame[:, :, 0] * 0.299) + (frame[:, :, 1] * 0.587) + (frame[:, :, 2] * 0.114)).astype(numpy.uint16)
+
 
 def frame_to_ascii(frame, colourmap):
 	"""
@@ -26,23 +36,171 @@ def frame_to_ascii(frame, colourmap):
 	# Formula for calcing ascii value stolen from stackoverflow
 	# colourmap length subtracted from 1 due to potential index errors.
 	# Once frame is converted the colourmap is applied to whole frame
-	frame = colourmap[((frame[:] * (len(colourmap)-1)) // 255).astype(numpy.uint8)]
+	frame = colourmap[((frame[:] * (len(colourmap) - 1)) // 255).astype(numpy.uint8)]
 	# Maps entire row to corresponding ascii character and adds that row to the string as 1 long string.
 	# frame is stiill in raw bytes so we need to decode it
 	return frame.tobytes().decode()
+
 
 def create_video_obj(video_file: str):
 	vr = cv.VideoCapture(video_file)
 	vr.set(cv.CAP_PROP_FPS, 2)
 	return vr
 
-load_dotenv()
 
-app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+def handle_mention(event, client, say):
+	print("Recieved: " + event["text"])
+	channel_id = event.get("channel")
+	thread_ts = event.get("thread_ts")
+	user_id = event.get("user")
+
+	if "slopify" not in event["text"].split():
+		print("No slopify")
+		return
+	if not thread_ts:
+		client.chat_postEphemeral(
+			channel=channel_id,
+			user=user_id,  # ID of the user who should see the message
+			thread_ts=thread_ts,  # Keeps the ephemeral message inside the thread
+			text="Mention me inside of the thread you want to slopify"
+		)
+		return
+
+	result = client.conversations_replies(channel=event["channel"], ts=thread_ts)
+	messages = result.get("messages", [])
+	return messages, thread_ts, user_id
+
+
+async def speak(text, name="output"):
+	communicate = edge_tts.Communicate(text)
+	await communicate.save(f"{name}.mp3")
+
+
+def upload_video(client, user_id, file_path, thread_ts):
+	dm = client.conversations_open(users=user_id)
+	try:
+		dm_channel_id = dm["channel"]["id"]
+		client.files_upload_v2(
+			channel=dm_channel_id,  # DMs to the user directly
+			file=file_path,
+			title=f"Slopified video {thread_ts}"
+		)
+	except Exception as e:
+		client.chat_postMessage(channel=user_id,
+								text=f"I'm to lazy to create actual error messages so here is what slack said went wrong.\n"
+									 f"{e}")
+		return
+	os.remove(file_path)
+
+
+@app.event("app_mention")
+def handle_slop_mention(event, client, say):
+	messages, thread_ts, user_id = handle_mention(event, client, say)
+	client.chat_postMessage(channel=user_id,
+							text="generating your video be patient as it can take a while")
+	text = ""
+	for message in messages:
+		p = r'https?://\S+|www\.\S+'
+		n_url = str(re.sub(p, "", message["text"]))
+		parts = re.split(r"<@(U[A-Z0-9]+)>", n_url)
+		for i in range(0, len(parts)):
+			if re.match(r"U[A-Z0-9]+", parts[i]):
+				parts[i] = client.users_info(user=parts[i])["user"]["profile"]["display_name"]
+		msg = "".join(parts)
+		text += msg + ". "
+
+	print("turning to audio")
+	name = f"{thread_ts}{random.randint(1000, 9999)}"
+	asyncio.run(speak(text, name))
+
+	print("turing to video")
+	audio_clip = AudioFileClip(f"{name}.mp3")
+
+	mypath = "slop_videos"
+	onlyfiles = [f for f in os.listdir(mypath) if os.path.isfile(os.path.join(mypath, f))]
+	file_i = random.randint(0, len(onlyfiles) - 1)
+
+	video_clip = (
+		VideoFileClip(f"{mypath}/{onlyfiles[file_i]}")
+		.with_volume_scaled(0)
+	).with_effects([vfx.Loop(duration=audio_clip.duration)])
+
+	video_clip.audio = audio_clip.subclipped(0, video_clip.duration)
+	video_clip.write_videofile(f"{name}.mp4")
+	os.remove(f"{name}.mp3")
+
+	threading.Thread(target=upload_video, args=(client, user_id, f"{name}.mp4", thread_ts)).start()
+
+
+@app.command("/slopify")
+def handle_slop_command(ack, say, command):
+	print("Triggered")
+	ack()
+
+	client = app.client
+
+	user_id = command["user_id"]
+	url = command.get("text", "")
+	client.chat_postMessage(channel=user_id,
+							text="generating your video be patient as it can take a while")
+	channel_match = re.search(r'/archives/(\w+)/', url)
+	channel_id = channel_match.group(1) if channel_match else None
+
+	# Extract Message TS
+	ts_match = re.search(r'/p(\d{16})', url)
+	if ts_match:
+		raw_ts = ts_match.group(1)
+		# Format: 1621234567.000123
+		thread_ts = f"{raw_ts[:-6]}.{raw_ts[-6:]}"
+
+	try:
+		result = client.conversations_replies(channel=channel_id, ts=thread_ts)
+		messages = result.get("messages", [])
+	except Exception as e:
+		client.chat_postMessage(channel=user_id,
+								text=f"I'm to lazy to create actual error messages so here is what slack said went wrong.\n"
+									 f"{e}")
+		return
+
+	text = ""
+	for message in messages:
+		p = r'https?://\S+|www\.\S+'
+		n_url = str(re.sub(p, "", message["text"]))
+		parts = re.split(r"<@(U[A-Z0-9]+)>", n_url)
+		for i in range(0, len(parts)):
+			if re.match(r"U[A-Z0-9]+", parts[i]):
+				parts[i] = client.users_info(user=parts[i])["user"]["profile"]["display_name"]
+		msg = "".join(parts)
+		text += msg + ". "
+
+	print("turning to audio")
+	name = f"{thread_ts}{random.randint(1000, 9999)}"
+	asyncio.run(speak(text, name))
+
+	print("turing to video")
+	audio_clip = AudioFileClip(f"{name}.mp3")
+
+	mypath = "slop_videos"
+	onlyfiles = [f for f in os.listdir(mypath) if os.path.isfile(os.path.join(mypath, f))]
+	file_i = random.randint(0, len(onlyfiles) - 1)
+
+	video_clip = (
+		VideoFileClip(f"{mypath}/{onlyfiles[file_i]}")
+		.with_volume_scaled(0)
+	).with_effects([vfx.Loop(duration=audio_clip.duration)])
+
+	video_clip.audio = audio_clip.subclipped(0, video_clip.duration)
+	video_clip.write_videofile(f"{name}.mp4")
+	os.remove(f"{name}.mp3")
+
+	threading.Thread(target=upload_video, args=(client, user_id, f"{name}.mp4", thread_ts)).start()
+
 
 @app.event("message")
 def handle_message_events(ack):
+	# Simply acknowledge to stop the "unhandled request" warning
 	pass
+
 
 @app.command("/generatecat")
 def handle_cat_gen_command(ack, say, command):
@@ -50,11 +208,10 @@ def handle_cat_gen_command(ack, say, command):
 	t1 = time.time()
 	ack()
 
-
 	print("recieved!")
 	client = app.client
 
-
+	# Extract info from the command payload
 	user_id = command["user_id"]
 	nums = command.get("text", "")
 
@@ -68,7 +225,7 @@ def handle_cat_gen_command(ack, say, command):
 
 	mypath = "cat"
 	onlyfiles = [f for f in os.listdir(mypath) if os.path.isfile(os.path.join(mypath, f))]
-	file_i = random.randint(0,len(onlyfiles)-1)
+	file_i = random.randint(0, len(onlyfiles) - 1)
 	img = cv.imread(f"cat/{onlyfiles[file_i]}")
 	img = cv.resize(img, (width, height))
 
@@ -76,7 +233,7 @@ def handle_cat_gen_command(ack, say, command):
 	lut = numpy.frombuffer(ASCII_COLOURMAP.encode(), dtype=numpy.uint8)
 	img_vals = frame_to_gs(img)
 	img = frame_to_ascii(img_vals, lut)
-	img = [(img[i:i+width]) for i in range(0, len(img), width)]
+	img = [(img[i:i + width]) for i in range(0, len(img), width)]
 	i = 0
 	length = 0
 	msg = ""
@@ -95,12 +252,15 @@ def handle_cat_gen_command(ack, say, command):
 		say(text=f"```{e}```")
 	print(time.time() - t1)
 
+
 @app.command("/badapple")
 def handle_badapple_command(ack, say, command):
+	# Acknowledge the command within 3 seconds
 	ack()
 
 	client = app.client
 
+	# Extract info from the command payload
 	user_id = command["user_id"]
 	nums = command.get("text", "")
 
